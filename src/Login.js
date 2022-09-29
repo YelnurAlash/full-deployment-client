0b76420ce9a3545b8400e66da9d4497dc47de315
Yelnur <zhazityelnur@gmail.com>
2022-09-29 14:30:24 +0600

    redirect

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import LoginBtn from './LoginBtn'
 import {useAuthState} from 'react-firebase-hooks/auth';
 import {auth} from './firebase'
 import LogoutBtn from './LogoutBtn';
 
 const Login = () => {
+    const [username, setUsername] = useState('');
     const [user] = useAuthState(auth);
     console.log(user);
+
+    const handleSubmit = async (e) => {
+        const response = await fetch('/', {
+            method: 'POST',
+            body: new FormData(e.target)
+        })
+    }
 
     return (
         <div>
             {user ? <LogoutBtn /> : <LoginBtn />}
+            <form onSubmit={handleSubmit}>
+                <input onChange={e => setUsername(e.target.value)} name='username'/>
+                <button type='submit'>Login</button>
+            </form>
         </div>
     )
 }
